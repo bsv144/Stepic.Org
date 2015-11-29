8f0fd2c0760944f97eaaf3bf5fc2b694ba5480a9
bsv144 <bsv_144@mail.ru>
2015-11-29 09:28:43 +0500

    Task 2

---
+++ Algorithm/Lession1/Fibonnachy3.py
@@ -0,0 +1,23 @@
+﻿import math 
+
+def fib_mod(n, m):
+    # a = 0
+    # b = 1
+    # c = 0
+    # if n == 1:
+        # c = 1
+    # for i in range(1,n,1):
+        # c = a + b
+        # a = b
+        # b = c
+    c = (math.pow(((1+math.sqrt(5))/2),n) - math.pow(((1-math.sqrt(5))/2),n))/math.sqrt(5)
+    return c%m
+
+
+def main():
+    n, m = map(int, input().split())
+    print(fib_mod(n, m))
+
+
+if __name__ == "__main__":
+    main()
